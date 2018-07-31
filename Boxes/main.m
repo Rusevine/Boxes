f4963e345f3da639da97fa8581af95293bcf8f96
Wiljay <wiljay.flores@gmail.com>
2018-07-31 11:43:06 -0400

    created 2 instances of box and calculated their volume in main

--- Boxes/main.m
+++ Boxes/main.m
@@ -7,11 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Box.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Box *b1 = [[Box alloc] initWithlength:10 width:10 height:10];
+        Box *b2 = [[Box alloc] initWithlength:20 width:20 height:20];
+        
+        float v1 = [b1 volume];
+        float v2 = [b2 volume];
+        
+        NSLog(@"%0.2f",v1);
+        NSLog(@"%0.2f",v2);
     }
     return 0;
 }
